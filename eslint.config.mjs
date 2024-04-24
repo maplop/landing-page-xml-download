84609000a1116e087d91fbb1a61cb39d0772ee75
Manuel <manuelalejandro9604@gmail.com>
2024-04-24 14:54:57 -0400

    chore(eslint): adjust configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,18 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'react/prop-types': 'off',
+      'no-console': 'warn',
+      'import/no-unresolved': 'error',
+      'react/jsx-uses-react': 'warn',
+      'import/no-unused-modules': 'warn',
+    },
+  },
+
   pluginJs.configs.recommended,
   pluginReactConfig,
 ];
