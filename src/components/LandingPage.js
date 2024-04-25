9c0962f2e0b5c406ed4d45347f89adc00e986ce8
Manuel <manuelalejandro9604@gmail.com>
2024-04-24 21:58:04 -0400

    feat: create SectionTitle component

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,12 @@
 import Navbar from "./Navbar/Navbar";
+import SectionTitle from "./SectionTitle/SectionTitle";
 
 function LandingPage() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <SectionTitle title="¿Estás listo para dar un gran paso en tu trabajo?" />
+    </>
   );
 }
 export default LandingPage;
