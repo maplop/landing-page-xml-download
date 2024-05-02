de0bcab6eaaff95cbe929ff17d0a88389849a4f3
Manuel <manuelalejandro9604@gmail.com>
2024-05-02 15:34:10 -0400

    feat: add hero background

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -5,15 +5,23 @@ import { styled } from "@mui/material";
 
 const HeroSection = () => {
   return (
-    <SectionContainer>
-      <SectionContent>
-        <Navbar />
-        <HeroContent />
-      </SectionContent>
-    </SectionContainer>
+    <Wrapper>
+      <SectionContainer>
+        <SectionContent>
+          <Navbar />
+          <HeroContent />
+        </SectionContent>
+      </SectionContainer>
+    </Wrapper>
   );
 };
 export default HeroSection;
+
+const Wrapper = styled("div")(({}) => ({
+  background: "linear-gradient(180deg, #FFF 0%, #E2F1FF 100%)",
+  clipPath: "polygon(0 0, 100% 0, 100% 85%, 0 100%)",
+  paddingBottom: 60,
+}));
 
 const SectionContent = styled("div")(({}) => ({
   paddingTop: 24,
