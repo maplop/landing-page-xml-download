a39c9bfb9c49257c21832211cf484f0dd3cd7ad1
Manuel <manuelalejandro9604@gmail.com>
2024-05-02 16:06:54 -0400

    feat: section two

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,5 +1,6 @@
 import HeroSection from "./HeroSection/HeroSection";
 import SectionOne from "./SectionOne/SectionOne";
+import SectionTwo from "./SectionTwo/SetionTwo";
 import SectionThree from "./SectionThree/SectionThree";
 import SectionFour from "./SectionFour/SectionFour";
 import SectionFive from "./SectionFive/SectionFive";
@@ -9,6 +10,7 @@ const LandingPage = () => {
     <>
       <HeroSection />
       <SectionOne />
+      <SectionTwo />
       <SectionThree />
       <SectionFour />
       <SectionFive />
